d2a2071770e4b70b5eab33c054ba290f5ea4a6df
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-16 19:57:47 +0100

    FINE

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var cognomi = ['Pesce', 'Castenetto', 'Passaro', 'Grimaldi', 'Rossi', 'Verdi', 'Bianchi' ];
-for (var i = 0; i < 1; i++) {
-  var cognomeUtente = prompt('Inserisci il tuo cognome')
-  cognomi.push(cognomeUtente);
-};
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+cognomi.push(cognomeUtente);
 cognomi.sort();
 document.getElementById("lista_cognomi").innerHTML = cognomi;
 var position = cognomi.indexOf(cognomeUtente)+1;
